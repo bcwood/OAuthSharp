635451be9f032fc188b2a686ab594c7c8dd54209
Brandon Wood <bcwood@gmail.com>
2014-10-22 11:55:07 -0800

    added signatureMethod param to test so it could be used to test both methods

--- OAuthSharp.Tests/v1/OAuth1ClientTests.cs
+++ OAuthSharp.Tests/v1/OAuth1ClientTests.cs
@@ -5,10 +5,12 @@ namespace OAuthSharp.Tests
     [TestFixture]
     public class OAuth1ClientTests
     {
-        [Test]
-        public void AcquireRequestToken_ResponseContains_TokenAndTokenSecret()
+        [TestCase(OAuth1Request.SIGNATURE_METHOD_PLAINTEXT, TestName = "PLAINTEXT")]
+		[TestCase(OAuth1Request.SIGNATURE_METHOD_HMAC_SHA1, TestName = "HMAC-SHA1")]
+        public void AcquireRequestToken_ResponseContains_TokenAndTokenSecret(string signatureMethod)
         {
 			var request = new OAuth1TokenRequest(TestData.CONSUMER_KEY, TestData.CONSUMER_SECRET, TestData.CALLBACK_URL);
+	        request.SignatureMethod = signatureMethod;
 
             var client = new OAuth1Client();
 			var response = client.AcquireRequestToken(TestData.OAUTH_URL_GET_REQUEST_TOKEN, request);
